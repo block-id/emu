39abe5cff7bd80c3dbbda3173fc9f7030a50abb3
areebbeigh <areebbeigh@gmail.com>
2022-01-01 13:47:31 +0530

    backend: Setup custom user model

--- backend/wallet/settings.py
+++ backend/wallet/settings.py
@@ -90,6 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+AUTH_USER_MODEL = "core.User"
 
 
 # Internationalization
